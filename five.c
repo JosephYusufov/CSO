c8c844b93a89681c89b76b513f36e840376aa57f
Joseph Yusufov <yusufovjoseph@gmail.com>
2021-03-14 12:16:37 -0400

    finished 5

--- five.c
+++ five.c
@@ -11,12 +11,50 @@ int main(int argc, char *argv[]){
     char *dup = argv[3];
     char c;
 
-    while(c = *(argv[1]++)){
+    // for each letter in str
+    while(c = *(str++)){
+        int e = 0;
+        int i = 0;
+        char echar = *except; 
+
+        // Return an error if the letter is not 'legal'
         if(!(c >= 97 && c <= 122)){
             printf("Error: illegal string.\n");
             return 1; 
         }
 
-        
+        // check if the letter appears in 'except'
+        while(echar != '\0'){
+            if(c == echar){
+                e = 1;
+            }
+            i++;
+            echar = *(except+i);
+        }
+
+        // if the letter DOES NOT appear in except
+        if(e == 0){
+            int d = 0;
+            char dchar = *dup;
+            i = 0;
+
+            // check if the letter appears in 'dup'
+            while(dchar != '\0'){
+                if(c == dchar){
+                    d = 1;
+                }
+                i++;
+                dchar = *(dup+i);
+            }
+
+            // if the letter appears in dup, print twice. Else, print once.
+            if(d){
+                putchar(c);
+                putchar(c);
+            } else {
+                putchar(c);
+            }
+        }
     }
+    putchar('\n');
 }
